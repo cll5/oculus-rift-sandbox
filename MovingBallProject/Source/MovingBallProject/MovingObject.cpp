48e9fa4f45dffded198fa90c8add5c8821d2ea24
cll5 <chuck.loong.lee@gmail.com>
2015-10-15 20:52:01 -0700

    added moving object behaviour functionalities

---
+++ MovingBallProject/Source/MovingBallProject/MovingObject.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingBallProject.h"
+#include "MovingObject.h"
+
+
+// Sets default values
+AMovingObject::AMovingObject()
+{
+	position = GetActorLocation();
+	velocity = FVector(0.0, 0.0, 0.0);
+	acceleration = FVector(0.0001, 0.0, 0.0);
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMovingObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMovingObject::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+	Move();
+}
+
---
+++ MovingBallProject/Source/MovingBallProject/MovingObject.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "MovingObject.generated.h"
+
+UCLASS()
+class MOVINGBALLPROJECT_API AMovingObject : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	FVector position;
+	FVector velocity;
+	FVector acceleration;
+
+protected:
+	virtual void updatePosition() {
+		velocity += acceleration;
+		position += velocity;
+	};
+
+public:	
+	// Sets default values for this actor's properties
+	AMovingObject();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	virtual void Move() {
+		updatePosition();
+	};
+	
+};
